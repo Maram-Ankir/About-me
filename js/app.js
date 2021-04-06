b21fc21eddb477f4a03f9fcfd7a16a3b5b7e1f46
Maram-Ankir <maramankir5@gmail.com>
2021-04-06 11:49:07 +0300

    editing

--- js/app.js
+++ js/app.js
@@ -11,12 +11,15 @@ let ques =prompt('Do you know my full name?');
 ques = ques.toLowerCase();
  switch(ques){
     case 'yes':  
-    case 'no':
-        alert('Im Maram Ankir');
+    case 'y':
+        alert('correct Im Maram Ankir');
+      break; 
+      case 'no':
+      case 'n':
+        alert('not correct try again');
       break; 
       default:
- alert('please enter yes or no');
- alert('Im Maram Ankir');
+      alert('please enter yes or no');
  break;
 }
 
@@ -30,9 +33,16 @@ let ques2 =prompt('Do you know my Age?');
 ques2 = ques2.toLowerCase();
  switch(ques2){
     case 'yes':  
-    case 'no':
-        alert('Im 22 years old');
+    case 'y':
+        alert('correct Im 22 years old');
       break;
+      case 'no':
+        case 'n':
+          alert('not correct try again');
+        break; 
+        default:
+        alert('please enter yes or no');
+   break;
 }
 console.log(ques2) ;
 
@@ -42,9 +52,16 @@ let ques3 =prompt('Do you know my favarite color?');
 ques3 = ques3.toLowerCase();
  switch(ques3){
     case 'yes':  
-    case 'no':
-        alert('I like blue the best');
-      break; 
+    case 'y':
+        alert('correct I like blue the best');
+      break;
+      case 'no':
+        case 'n':
+          alert('not correct try again');
+        break; 
+        default:
+        alert('please enter yes or no');
+   break; 
 }
 console.log(ques3) ;
 
@@ -53,9 +70,16 @@ let ques4 =prompt('Do you know in which university I graduated from ?');
 ques4 = ques4.toLowerCase();
  switch(ques4){
     case 'yes':  
-    case 'no':
-        alert('I graduated from AL-Balqa Applied university');
+    case 'y':
+        alert('correct I graduated from AL-Balqa Applied university');
       break; 
+      case 'no':
+        case 'n':
+          alert('not correct try again');
+        break; 
+        default:
+        alert('please enter yes or no');
+   break;
 }
 console.log(ques4) ;
 
@@ -65,9 +89,16 @@ let ques5 =prompt('Do I like food ?');
 ques5 = ques5.toLowerCase();
  switch(ques5){
     case 'yes':  
-    case 'no':
-        alert('of course I do =P');
+    case 'y':
+        alert('correct of course I do =P');
       break; 
+      case 'no':
+        case 'n':
+          alert('not correct try again');
+        break; 
+        default:
+        alert('please enter yes or no');
+   break;
 }
 console.log(ques5) ;
 console.log ('welcome again');
